5ceb8574cfd3b1dc9987e905ba20d1b2ad1f2ac6
victorramos04 <117530384+victorramos04@users.noreply.github.com>
2022-11-05 11:43:32 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -12,16 +12,3 @@ function pay() {
     window.alert(products.join(", \n"));
 }
 
-    $(".producto").click(function () {
-        //console.log("entro");
-        //traer todo el contenido
-        let contenido= $(this).html();
-        //traer solo la img
-        let imagen=$(".img",this).html();
-        console.log(imagen);
-        $(".popup").html(contenido);        
-        $(".popup , .popup .description , .popup button").show();
-    });
-    function cerrar() {     
-        $(".popup").hide();
-    };
